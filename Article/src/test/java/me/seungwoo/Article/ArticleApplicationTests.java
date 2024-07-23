689ea566763b1957bd335d7750f9cd2c724ee2e3
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2024-07-23 16:26:09 +0900

    init: init project

---
+++ Article/src/test/java/me/seungwoo/Article/ArticleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seungwoo.Article;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
