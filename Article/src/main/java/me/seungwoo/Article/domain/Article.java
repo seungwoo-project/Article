1d6a4ce1ea505acea2463e815193593bfe960d17
이승우 <oltmddn123@naver.com>
2024-07-24 11:41:43 +0900

    Feat: 블로그 글 등록 구현

--- Article/src/main/java/me/seungwoo/Article/domain/Article.java
+++ Article/src/main/java/me/seungwoo/Article/domain/Article.java
@@ -4,9 +4,11 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
